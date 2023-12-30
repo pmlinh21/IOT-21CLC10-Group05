adf2d39ef0e2f86c05e185e333e6e6e0f7cd10e5
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2023-12-30 12:34:57 +0700

    Xong mqtt in, con mqtt out

--- test/test.ino
+++ test/test.ino
@@ -13,15 +13,19 @@ DHT dht(DHT_PIN, DHT_TYPE);
 #define MOTOR_PIN_1 25
 #define MOTOR_PIN_2 26
 
+float defaultTemperature = 35;  // Nhiệt độ mặc định tự động kéo mái che
+float defaultHumidity = 80;     // Độ ẩm mặc định tự động báo khẩn cấp
+
 float temperature;          // Đọc từ DHT11
 float humidity;             // Đọc từ DHT11
 int rain;                   // Đọc từ Rain Sensor - HIGH / LOW
 
-boolean isOpen = false;               // Mái che đang mở
-boolean isRaining = false;            // Trời đang mưa     (rain = HIGH)
-boolean isHot = false;                // Trời nắng nóng    (temperature > 35)
-boolean isSafe = true;                // Mạch điện an toàn (humidity < 80)
-int mode = 1;                         // 1: Chế độ tự động "Bình thường", 2: Chế độ tự động "Phơi đồ", 3: Người dùng tự điều khiển
+// Giá trị mặc định ban đầu
+bool isOpen = false;                // Mái che đang đóng
+bool isRaining = false;             // Trời không mưa     (rain = HIGH)
+bool isHot = false;                 // Trời không nóng    (temperature > 35)
+bool isSafe = true;                 // Mạch điện an toàn (humidity < 80)
+int mode = 1;                       // 1: Chế độ tự động "Bình thường", 2: Chế độ tự động "Phơi đồ", 3: Người dùng tự điều khiển
 
 // Wifi Cafe
 const char* ssid = "The Simple Cafe";
@@ -30,6 +34,10 @@ const char* password = "simpleisthebest";
 // Wifi Lien
 // const char* ssid = "YenLien";
 // const char* password = "ngoquocanh";
+
+// Wifi 3G
+// const char* ssid = "21127341";
+// const char* password = "hihihaha";
 
 // Wifi Thư viện
 // const char* ssid = "HCMUS Thu Vien";
@@ -56,13 +64,13 @@ void mqttReconnect() {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("21127341")) {
       Serial.println(" connected");
-      client.subscribe("project/openMaiche");
+      client.subscribe("project/userOpenMaiche");
 
       // subcribe other topics here
 
     } else {
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
+      Serial.println(" try again in 2 seconds");
+      delay(2000);
     }
   }
 }
@@ -75,18 +83,24 @@ void callback(char* topic, byte* message, unsigned int length) {
 
   // Chế độ 3
   if (strcmp(topic, "project/userOpenMaiche")) {
-    if (stMessage == "Mở") {
-      Serial.println("Người dùng mở mái che");
-      openMaiche();
-    }
-    else if (stMessage == "Đóng") {
-      Serial.println("Người dùng đóng mái che");
-      closeMaiche();
-    }
-    else {
-      Serial.println("Sai topic hoặc sai message");
-    }
+    Serial.println(stMessage);
+    // if (stMessage == "Mở") {
+    //   Serial.println("Người dùng mở mái che");
+    //   openMaiche();
+    // }
+    // else if (stMessage == "Đóng") {
+    //   Serial.println("Người dùng đóng mái che");
+    //   closeMaiche();
+    // }
+    // else {
+    //   Serial.println("Sai topic hoặc sai message");
+    // }
   }
+
+  // Người dùng điều chỉnh nhiệt độ & độ ẩm mặc định
+  // if(strcmp(topic, "project/changeDefaultValues")) {
+  //   if (stMessage == ) 
+  // }
 }
 
 void setup(){
@@ -100,14 +114,16 @@ void setup(){
   digitalWrite(MOTOR_PIN_2,LOW);
 
   dht.begin();
-
+  
+  pinMode(GPIO_NUM_2, OUTPUT);
+  digitalWrite(GPIO_NUM_2, HIGH);
   wifiConnect();
 
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
   client.setServer(mqttServer, port);
-  client.setCallback(callback);
+  // client.setCallback(callback);
 }
 
 void closeMaiche(){
@@ -135,17 +151,19 @@ void loop(){
   int rain = digitalRead(23);
 
   isRaining = (rain == LOW);
-  isHot = (temperature > 35);
-  isSafe = (humidity < 80);
 
   // Message publish to MQTT
-  char buffer[100];
+  char buffer[200];
   if (isnan(temperature) || isnan(humidity)) {
+    isHot = false;
+    isSafe = true;
     Serial.println("Failed to read from DHT sensor!");
-    sprintf(buffer, "{\"temperature\":null, \"humidity\":null, \"isOpen\":\"%s\", \"isRaining\":\"%s\", \"isHot\":\"%s\", \"isSafe\":\"%s\", \"mode\":%d}", isOpen ? "true" : "false", isRaining ? "true" : "false", isHot ? "true" : "false", isSafe ? "true" : "false", mode);
+    sprintf(buffer, "{\"defaultTemperature\":%.0f, \"defaultHumidity\":%.0f, \"temperature\":null, \"humidity\":null, \"isOpen\":%s, \"isRaining\":%s, \"isHot\":%s, \"isSafe\":%s, \"mode\":%d}", defaultTemperature, defaultHumidity, isOpen ? "true" : "false", isRaining ? "true" : "false", isHot ? "true" : "false", isSafe ? "true" : "false", mode);
   }
   else {
-    sprintf(buffer, "{\"temperature\":%2.f, \"humidity\":%2.f, \"isOpen\":\"%s\", \"isRaining\":\"%s\", \"isHot\":\"%s\", \"isSafe\":\"%s\", \"mode\":%d}", isOpen ? "true" : "false", isRaining ? "true" : "false", isHot ? "true" : "false", isSafe ? "true" : "false", mode);
+    isHot = (temperature > 35);
+    isSafe = (humidity < 80);
+    sprintf(buffer, "{\"defaultTemperature\":%.0f, \"defaultHumidity\":%.0f, \"temperature\":%.1f, \"humidity\":%.0f, \"isOpen\":%s, \"isRaining\":%s, \"isHot\":%s, \"isSafe\":%s, \"mode\":%d}", defaultTemperature, defaultHumidity, temperature, humidity, isOpen ? "true" : "false", isRaining ? "true" : "false", isHot ? "true" : "false", isSafe ? "true" : "false", mode);
   }
   Serial.println(buffer);
   client.publish("project/data", buffer);
